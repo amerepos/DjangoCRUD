3bf84e76db28521fee6458613433663b9532c0e6
amerepos <ahmad.tarek.amer@gmail.com>
2022-06-18 17:12:07 +0200

    Oauth Choice Vew

--- crud_framework/views/oauth.py
+++ crud_framework/views/oauth.py
@@ -1,5 +1,11 @@
 from oauth2_provider.views.mixins import ProtectedResourceMixin, OAuthLibMixin
 from .base import method_decorator, csrf_exempt, view_catch_error, BaseCrudView, BaseView
+from .lookups import ChoicesView
+
+
+@method_decorator([csrf_exempt, view_catch_error], name='dispatch')
+class OauthChoiceView(ProtectedResourceMixin, OAuthLibMixin, ChoicesView):
+    pass
 
 
 @method_decorator([csrf_exempt, view_catch_error], name='dispatch')
