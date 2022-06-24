9540b7bc0a1edcbfa287699cd1324d78d952a651
amerepos <ahmad.tarek.amer@gmail.com>
2022-06-24 18:52:11 +0200

    Set user in Oauth view

--- crud_framework/views/oauth.py
+++ crud_framework/views/oauth.py
@@ -3,16 +3,52 @@ from .base import method_decorator, csrf_exempt, view_catch_error, BaseCrudView,
 from .lookups import ChoicesView
 
 
+# def set_user(f): TODO
+#     def wrap(self, request, *args, **kwargs):
+#         self.user = request.resource_owner
+#         return f(self=self, request=request, *args, **kwargs)
+#
+#     wrap.__doc__ = f.__doc__
+#     wrap.__name__ = f.__name__
+#     return wrap
+
+
+class SetUserMixin:
+    def __init__(self, *args, **kwargs):
+        super(SetUserMixin, self).__init__(*args, **kwargs)
+        self.user = None
+
+    def get(self, request, *args, **kwargs):
+        self.user = request.resource_owner
+        print(f'USER: {self.user}')
+        return super(SetUserMixin, self).get(request=request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.user = request.resource_owner
+        print(f'USER: {self.user}')
+        return super(SetUserMixin, self).get(request=request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        self.user = request.resource_owner
+        print(f'USER: {self.user}')
+        return super(SetUserMixin, self).get(request=request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        self.user = request.resource_owner
+        print(f'USER: {self.user}')
+        return super(SetUserMixin, self).get(request=request, *args, **kwargs)
+
+
 @method_decorator([csrf_exempt, view_catch_error], name='dispatch')
-class OauthChoiceView(ProtectedResourceMixin, OAuthLibMixin, ChoicesView):
+class OauthChoiceView(ProtectedResourceMixin, OAuthLibMixin, SetUserMixin, ChoicesView):
     pass
 
 
 @method_decorator([csrf_exempt, view_catch_error], name='dispatch')
-class OauthCrudView(ProtectedResourceMixin, OAuthLibMixin, BaseCrudView):
+class OauthCrudView(ProtectedResourceMixin, OAuthLibMixin, SetUserMixin, BaseCrudView):
     pass
 
 
 @method_decorator([csrf_exempt, view_catch_error], name='dispatch')
-class OauthFunctionalView(ProtectedResourceMixin, OAuthLibMixin, BaseView):
+class OauthFunctionalView(ProtectedResourceMixin, OAuthLibMixin, SetUserMixin, BaseView):
     pass
