75c1f23d8b48da0ea1b416b23389714e3b2da2c0
amerepos <ahmad.tarek.amer@gmail.com>
2022-08-13 01:23:15 +0200

    Permissions mixin

--- crud_framework/schemas/user_aware.py
+++ crud_framework/schemas/user_aware.py
@@ -36,3 +36,50 @@ class UserAwareCrudSchema(CrudSchema):
 
     def delete(self, **data):  # TODO make sure user can delete
         return super(UserAwareCrudSchema, self).delete(editor=self.user, **data)
+
+
+class PermissionMixin:
+    VIEW_PERM = None
+    ADD_PERM = None
+    EDIT_PERM = None
+    DELETE_PERM = None
+
+    def __init__(self, *args, **kwargs):
+        super(PermissionMixin, self).__init__(*args, **kwargs)
+        self.app_label = self.MODEL_CLASS._meta.app_label
+
+    def get(self, *args, **kwargs):
+        if not self.VIEW_PERM:
+            self.VIEW_PERM = f"{self.app_label}.view_{self.model_name}"
+
+        if self.user.has_perm(self.VIEW_PERM):
+            return super(PermissionMixin, self).get(*args, **kwargs)
+        else:
+            raise Error(status=HttpStatus.HTTP_403_FORBIDDEN, field_name='user', message='You can\'t view this item!')
+
+    def post(self, *args, **kwargs):
+        if not self.ADD_PERM:
+            self.ADD_PERM = f"{self.app_label}.add_{self.model_name}"
+
+        if self.user.has_perm(self.ADD_PERM):
+            return super(PermissionMixin, self).post(*args, **kwargs)
+        else:
+            raise Error(status=HttpStatus.HTTP_403_FORBIDDEN, field_name='user', message='You can\'t create this item!')
+
+    def put(self, *args, **kwargs):
+        if not self.EDIT_PERM:
+            self.EDIT_PERM = f"{self.app_label}.change_{self.model_name}"
+
+        if self.user.has_perm(self.EDIT_PERM):
+            return super(PermissionMixin, self).put(*args, **kwargs)
+        else:
+            raise Error(status=HttpStatus.HTTP_403_FORBIDDEN, field_name='user', message='You can\'t edit this item!')
+
+    def delete(self, *args, **kwargs):
+        if not self.DELETE_PERM:
+            self.DELETE_PERM = f"{self.app_label}.delete_{self.model_name}"
+
+        if self.user.has_perm(self.DELETE_PERM):
+            return super(PermissionMixin, self).delete(*args, **kwargs)
+        else:
+            raise Error(status=HttpStatus.HTTP_403_FORBIDDEN, field_name='user', message='You can\'t delete this item!')
